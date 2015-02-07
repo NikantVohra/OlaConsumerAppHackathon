103facbe50c31fecec7e794317f2ebeb307e37e3
nvohra <nvohra@paypal.com>
2015-02-07 20:55:41 +0530

    Initial Commit

---
+++ OlaChaloNiklo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OlaChaloNiklo
+//
+//  Created by Vohra, Nikant on 07/02/15.
+//  Copyright (c) 2015 Vohra, Nikant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
